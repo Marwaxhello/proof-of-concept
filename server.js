5e99ed53e7b4f75c8115232cff128c871daa6385
Marwaxhello <marwahello33@gmail.com>
2023-06-17 11:50:23 +0200

    Head + Footer styling

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import express from "express";
+import express, { response } from "express";
 import * as path from "path";
 import fetch from "node-fetch";
-
 
 const server = express();
 
@@ -19,10 +18,31 @@ server.get("/", (req, res) => {
   res.render("index");
 });
 
-
 async function dataFetch(url) {
   const data = await fetch(url)
     .then((response) => response.json())
     .catch((error) => error);
   return data;
 }
+
+// server.get("/", (req, res) => {
+//   let itemUrl = url + "/items/activities";
+//   dataFetch(itemUrl).then((data) => {
+//     console.log(data);
+//     response.render("index", { data: data });
+//   });
+// });
+
+// async function dataFetch(url) {
+//   const data = await fetch(url, {
+//     headers: {
+//       Authorization: "Bearer -xWJUqFTn3NSpbRIMxytzqlCVs8WF8rLB",
+//     },
+//   })
+//     .then((response) => response.json())
+
+//     .catch((error) => error);
+
+//   return data;
+// }
+
