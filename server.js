62bb8e49b79180db57079fcd911fba5fa995c07a
Marwaxhello <marwahello33@gmail.com>
2023-06-18 21:49:40 +0200

    Data geïmplementeerd

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ import * as path from "path";
 import fetch from "node-fetch";
 
 const server = express();
-// const url =
-//   "https://api.fivespark.com/docs/?access_token=-xWJUqFTn3NSpbRIMxytzqlCVs8WF8rLB";
+const url = "https://api.fivespark.com";
 
 server.set("view engine", "ejs");
 server.set("views", "./views");
@@ -16,34 +15,28 @@ server.listen(server.get("port"), () => {
   console.log(`Application started on http://localhost:${server.get("port")}`);
 });
 
-server.get("/", (req, res) => {
-  res.render("index");
+server.get("/", async (req, res) => {
+  try {
+    const itemUrl = url + "/items/activities";
+    const { data } = await dataFetch(itemUrl);
+
+    res.render("index", { activities: data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
+
 async function dataFetch(url) {
-  const data = await fetch(url)
+  const data = await fetch(url, {
+    headers: {
+      Authorization: "Bearer xWJUqFTn3NSpbRIMxytzqlCVs8WF8rLB",
+    },
+  })
     .then((response) => response.json())
+
     .catch((error) => error);
+
   return data;
 }
-
-// server.get("/", (req, res) => {
-//   let itemUrl = url + "/items/activities";
-//   dataFetch(itemUrl).then((data) => {
-//     console.log(data);
-//     response.render("index", { data: data });
-//   });
-// });
-
-// async function dataFetch(url) {
-//   const data = await fetch(url, {
-//     headers: {
-//       Authorization: "Bearer -xWJUqFTn3NSpbRIMxytzqlCVs8WF8rLB",
-//     },
-//   })
-//     .then((response) => response.json())
-
-//     .catch((error) => error);
-
-//   return data;
-// }
